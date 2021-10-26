65003050b99397c3a28417cb3d10a60dd231bf0f
sandroteste72 <sandro.teste72@gmail.com>
2021-10-26 02:03:31 -0300

    Developed remove method

--- binary-tree/src/main/java/com/arpitech/BinaryTree.java
+++ binary-tree/src/main/java/com/arpitech/BinaryTree.java
@@ -26,4 +26,112 @@ public class BinaryTree<T extends Comparable<T>> {
         }
         return  current;
     }
+
+    public void displayInOrder() {
+        System.out.println("\n Displaying InOrder");
+        displayInOrder(this.root);
+    }
+
+    private void displayInOrder(BinaryNode<T> current) {
+        if (current != null) {
+            displayInOrder(current.getLeftNode());
+            System.out.println(current.getContents() + ", ");
+            displayInOrder(current.getRightNode());
+        }
+    }
+
+    public void displayPostOrder() {
+        System.out.println("\n Displaying PostOrder");
+        displayPostOrder(this.root);
+    }
+
+    private void displayPostOrder(BinaryNode<T> current) {
+        if (current != null) {
+            displayPostOrder(current.getLeftNode());
+            displayPostOrder(current.getRightNode());
+            System.out.println(current.getContents() + ", ");
+        }
+    }
+
+    public void displayPreOrder() {
+        System.out.println("\n Displaying PreOrder");
+        displayPreOrder(this.root);
+    }
+
+    private void displayPreOrder(BinaryNode<T> current) {
+        if (current != null) {
+            System.out.println(current.getContents() + ", ");
+            displayPreOrder(current.getLeftNode());
+            displayPreOrder(current.getRightNode());
+        }
+    }
+
+    public void remove(T contents) {
+        try {
+            BinaryNode<T> current = this.root;
+            BinaryNode<T> parent = null;
+            BinaryNode<T> child = null;
+            BinaryNode<T> temp = null;
+
+            while (current != null && !current.getContents().equals(contents)) {
+                parent = current;
+                if (contents.compareTo(current.getContents()) < 0) {
+                    current = current.getLeftNode();
+                } else {
+                    current = current.getRightNode();
+                }
+            }
+
+            if (current == null) {
+                System.out.println("Contents not fount! Try block");
+            }
+
+            if (parent == null){
+                if (current.getRightNode() == null) {
+                    this.root = current.getLeftNode();
+                } else if (current.getLeftNode() == null) {
+                    this.root = current.getRightNode();
+                } else {
+                    for (temp = current, child = current.getLeftNode();
+                        child.getRightNode() != null;
+                        temp = child, child = child.getLeftNode()) {
+                        if (child != child.getLeftNode()) {
+                            temp.setRightNode(child.getLeftNode());
+                            child.setLeftNode(root.getLeftNode());
+                        }
+                    }
+                    child.setRightNode(root.getRightNode());
+                    root = child;
+                }
+            } else if (current.getRightNode() == null) {
+                if (parent.getLeftNode() == current) {
+                    parent.setLeftNode(current.getLeftNode());
+                } else {
+                    parent.setRightNode(current.getLeftNode());
+                }
+            } else if (current.getLeftNode() == null) {
+                if (parent.getLeftNode() == current) {
+                    parent.setLeftNode(current.getRightNode());
+                } else {
+                    parent.setRightNode(current.getRightNode());
+                }
+            } else {
+                for (temp = current, child = current.getLeftNode(); child.getRightNode() != null; temp = child, child = child.getRightNode()) {
+                    if (child != current.getLeftNode()) {
+                        temp.setRightNode(child.getLeftNode());
+                        child.setLeftNode(current.getLeftNode());
+                    }
+                    child.setRightNode(current.getRightNode());
+                    if (parent.getLeftNode() == current) {
+                        parent.setLeftNode(child);
+                    } else {
+                        parent.setRightNode(child);
+                    }
+                }
+            }
+        }
+        catch (NullPointerException e) {
+            System.out.println("Contents not found! Catch block");
+        }
+    }
 }
